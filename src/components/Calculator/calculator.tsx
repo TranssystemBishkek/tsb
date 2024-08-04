25539dcf7de59861f26b6fa62bce86c9677eb788
Aibek Sadraliev <a.sadraliev@gmail.com>
2024-08-04 14:14:40 -0500

    fix: layour zindex

--- src/components/Calculator/calculator.tsx
+++ src/components/Calculator/calculator.tsx
@@ -49,7 +49,7 @@ export const Calculator = (props: Props) => {
   };
 
   return (
-    <section className=" relative -z-10 calculation-of-services">
+    <section className="relative calculation-of-services">
       <div className="container">
         <h2
           id="calculation-of-services"
